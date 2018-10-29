2d8b79248309e893c079d1a1d90d6795ee8145c5
Harsimranjit Sindhar <harsimransindhar@hotmail.com>
2018-10-29 13:22:20 -0400

    Update SHT30 Temp & rH/sht30.py

--- SHT30 Temp & rH/sht30.py
+++ SHT30 Temp & rH/sht30.py
@@ -8,15 +8,16 @@ import time
 bus = smbus.SMBus(1)
 
 # SHT30 address, 0x44(68), or 0x45
-# Send measurement command, 0x2C(44)
-#		0x06(06)	High repeatability measurement
+# Send measurement command,{0x2c, [0x06]} High repeatability measurement
+
 bus.write_i2c_block_data(0x45, 0x2C, [0x06])
 
+#wait
 time.sleep(0.5)
 
-# SHT30 address, 0x44(68)
-# Read data back from 0x00(00), 6 bytes
-# cTemp MSB, cTemp LSB, cTemp CRC, Humididty MSB, Humidity LSB, Humidity CRC
+# SHT30 address, 0x45
+# Read 6 bytes of data back from 0x00(00)
+# cTemp MSB,LSB,CRC, Humidity MSB,LSB,CRC
 data = bus.read_i2c_block_data(0x45, 0x00, 6)
 
 # Convert the data
